59445919ef18db3549e5723a63f7c373805f1cef
George Patterson <george.patterson@cottonwoodclassical.org>
2024-10-13 20:11:04 -0600

    third commit

--- blog/views.py
+++ blog/views.py
@@ -3,15 +3,15 @@ from django.shortcuts import render
 posts = [
     {
         'author': 'George patterson',
-        'title': 'how to stop you kids from whatching to much T.V.'
-        'content': 'If you have not let your kids whatch T.V. before you will have it easier because they do not know what it is so just do not show it to them'
-        'date_posted ': 'October 13, 2024'
+        'title': 'how to stop you kids from whatching to much T.V.',
+        'content': 'If you have not let your kids whatch T.V. before you will have it easier because they do not know what it is so just do not show it to them',
+        'date_posted': 'October 13, 2024'
     },
     {
-         'author': 'George patterson',
-        'title': 'how to stop you kids from whatching to much T.V.'
-        'content': 'If they do know what T.v. is than it will be trickier, but not impossible. (1) one trick that helps is to after a nap or sleeping you should not give them T.V, this is mainly for toddlers. (2) for older kids one trick is to teach them the value of playing out side or doing the chores in a way that makes them happy to do it.'
-        'date_posted ': 'October 13, 2024'
+        'author': 'George patterson',
+        'title': 'how to stop you kids from whatching to much T.V.',
+        'content': 'If they do know what T.v. is than it will be trickier, but not impossible. (1) one trick that helps is to after a nap or sleeping you should not give them T.V, this is mainly for toddlers. (2) for older kids one trick is to teach them the value of playing out side or doing the chores in a way that makes them happy to do it.',
+        'date_posted': 'October 13, 2024'
     } 
 ]
 
@@ -25,4 +25,4 @@ def home(request):
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
