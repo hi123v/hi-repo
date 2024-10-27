6a8fe63be9db0716281ac0c7a864fd76ef134c68
George Patterson <george.patterson@cottonwoodclassical.org>
2024-10-27 17:39:13 -0600

    commit number 6.9

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth.forms import UserCreationForm
 
 
 class UserRegisterForm(UserCreationForm):
@@ -8,4 +8,4 @@ class UserRegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'passwprd1', 'password2']
+        fields = ['username', 'email', 'password1', 'password2']
