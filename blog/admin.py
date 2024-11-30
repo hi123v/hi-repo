41939c46ca0e1fa637965414480a8dbc377fc325
George Patterson <george.patterson@cottonwoodclassical.org>
2024-11-29 19:46:06 -0700

    8.1

--- blog/admin.py
+++ blog/admin.py
@@ -2,3 +2,7 @@ from django.contrib import admin
 from .models import Post
 
 admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin): 
+    list_display = ('title', 'author', 'published_date') 
+    list_filter = ('author', 'published_date') 
+    search_fields = ('title', 'content')
